ad9d2b7222951028b09ab8a91012543bf02aaef8
mostafa-asefy <asefi.v@gmail.com>
2020-05-03 05:22:17 +0330

    Git initialized and Lesson 10 finished.

---
+++ a_defining_variables.jl
@@ -0,0 +1,24 @@
+#Defining Variables
+
+my_name = "Mostafa Asefy"
+println(my_name)
+
+#Re-assigning Variables
+my_name = "Abbas"
+println(my_name)
+
+#Numerical variables
+my_score = 19.5
+println(my_score)
+
+my_Sco♠re = 19.5
+println(my_score)
+
+α = 30
+println(α)
+
+#Julia keywords not allowed
+#if = "something"
+
+#UdefVarError your_school not defined
+print(your_school)
